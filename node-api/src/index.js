d486fd3bf2a93dbbc38998825853eb29824b9da1
gzitei <gustavo.zitei@gmail.com>
2024-11-09 17:02:32 -0300

    adds web api example using node std lib

---
+++ node-api/src/index.js
@@ -0,0 +1,135 @@
+import http from "node:http";
+import crypto from "node:crypto";
+import fs from "node:fs";
+
+const url = "localhost";
+const port = 3000;
+const salt = crypto.randomBytes(16).toString("hex");
+
+const router = {
+  GET: {},
+  POST: {},
+  PUT: {},
+  DELETE: {},
+};
+
+const db = "./db.json";
+
+const readUsers = () => {
+  const fileExists = fs.existsSync(db);
+  if (!fileExists) {
+    updateUsers({ users: [] });
+  }
+  return fs.readFileSync(db, { encoding: "utf-8" });
+};
+
+const updateUsers = (users) => {
+  fs.writeFileSync(db, JSON.stringify(users, null, 2), { encoding: "utf-8" });
+};
+
+const verifyPassword = (password, storedHash) => {
+  const hash = hashPassword(password);
+  return hash === storedHash;
+};
+
+const authenticateUser = (username, password) => {
+  const registeredUsers = readUsers();
+  const json = JSON.parse(registeredUsers);
+  const candidate = json.users.filter((user) => user.username === username);
+  if (candidate.length === 0) throw new Error("Usuário não encontrado!");
+  const validPassword = verifyPassword(password, candidate[0].password);
+  if (!validPassword) throw new Error("Senha inválida");
+  return `Usuário ${username} logado com sucesso!`;
+};
+
+const registerUser = (username, inputPassword) => {
+  const registeredUsers = readUsers();
+  const json = JSON.parse(registeredUsers);
+  const usernameAllowed = json.users.filter(
+    (user) => username === user.username,
+  );
+  if (usernameAllowed.length > 0) throw new Error("Nome de usuário já existe!");
+  const password = hashPassword(inputPassword);
+  const id = crypto.randomUUID();
+  const timestamp = new Date().toISOString();
+  json.users.push({
+    timestamp,
+    id,
+    username,
+    password,
+  });
+  updateUsers(json);
+};
+
+const hashPassword = (password) => {
+  const encrypted = crypto
+    .pbkdf2Sync(password, salt, 1000, 64, "sha512")
+    .toString("hex");
+  return encrypted;
+};
+
+const getBody = async (req) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  return JSON.parse(Buffer.concat(buffers).toString());
+};
+
+const defaultHandler = (req, res) => {
+  res.writeHead("404", { "Content-Type": "text/plain" });
+  res.end("Recurso não encontrado!");
+};
+
+const newUser = async (req, res) => {
+  const body = await getBody(req);
+  const { username, password } = body;
+  let statusCode, msg;
+  try {
+    registerUser(username, password);
+    statusCode = 200;
+    msg = `Usuário ${username} registrado!`;
+  } catch (e) {
+    statusCode = 400;
+    msg = e.message;
+  }
+  res.writeHead(statusCode, { "Content-Type": "text/plain" });
+  res.end(msg);
+};
+
+const login = async (req, res) => {
+  const body = await getBody(req);
+  const { username, password } = body;
+  let statusCode, msg;
+  try {
+    authenticateUser(username, password);
+    statusCode = 200;
+    msg = `Usuário ${username} autenticado!`;
+  } catch (e) {
+    statusCode = 400;
+    msg = e.message;
+    console.error(e.stack);
+  }
+  res.writeHead(statusCode, { "Content-Type": "text/plain" });
+  res.end(msg);
+};
+
+router["POST"]["/register"] = newUser;
+router["POST"]["/login"] = login;
+router["GET"]["/"] = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World!\n");
+};
+
+const server = http.createServer((req, res) => {
+  const endpoint = req.url;
+  const method = req.method;
+  const handler = router[method][endpoint] || defaultHandler;
+  handler(req, res);
+});
+
+server.listen(port, url, () => {
+  console.log(`Server running on ${url}:${port}`);
+});
